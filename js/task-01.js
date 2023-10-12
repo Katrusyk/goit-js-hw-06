7ec58ce2ced5f8ebe6ea2986eb45e767c2ba7e13
Katrusyk <katerynagavel@gmail.com>
2023-10-12 20:48:08 +0300

    edit first task

--- js/task-01.js
+++ js/task-01.js
@@ -1,11 +1,13 @@
 const categoriesList = document.getElementById('categories');
-const categoryItems = categoriesList.querySelectorAll('li.item');
+const categoryItems = categoriesList.childNodes;
 
 console.log(`Number of categories: ${categoryItems.length}`);
 
 categoryItems.forEach((categoryItem) => {
-  const categoryName = categoryItem.querySelector('h2').textContent;
-  const categoryElements = categoryItem.querySelectorAll('ul li').length;
-  console.log(`Category: ${categoryName}`);
-  console.log(`Elements: ${categoryElements}`);
+  if (categoryItem.nodeType === 1) {
+    const categoryName = categoryItem.querySelector('h2').textContent;
+    const categoryElements = categoryItem.querySelector('ul').children.length;
+    console.log(`Category: ${categoryName}`);
+    console.log(`Elements: ${categoryElements}`);
+  }
 });
